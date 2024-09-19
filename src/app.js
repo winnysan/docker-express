b936499fd5c7bdb682c77dd344ef5bb8e494a709
winnysan <maarvinko@gmail.com>
2024-09-19 11:18:23 +0200

    mongo

--- src/app.js
+++ src/app.js
@@ -1,10 +1,23 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 const port = 4000;
 
+const uri = process.env.MONGO_URI || "mongodb://localhost:27017/database";
+
+(async () => {
+  try {
+    const conn = await mongoose.connect(uri);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.log(`MongoDB Error: ${err}`);
+    process.exit(1);
+  }
+})();
+
 app.get("/", (req, res) => {
-  res.send("From Docker");
+  res.send("Express app with MongoDB from Docker");
 });
 
 app.listen(port, () => {
